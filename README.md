31a95fa435534729939a7ce3db2ea9ed5364e318
BigAN <ddongjian0000@gmail.com>
2015-07-30 10:36:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sampling-according-to-probability-distribution
---
+++ test_random.py
@@ -0,0 +1,28 @@
+#!/usr/env/bin python
+# coding=utf8
+from __future__ import unicode_literals
+
+__author__ = 'Dongjian'
+import random
+def random_distr(l):
+    r = random.uniform(0, sum([x[1] for x in l]))
+    s = 0
+    for num,(item, prob) in enumerate(l):
+        s += prob
+        if s >= r:
+            return num
+    return l[-1]
+
+
+
+l = [(4,0.4),(3,0.3),(2,0.2),(1,0.1)]
+
+def generate_random(l):
+    for _ in xrange(len(l)):
+        rrsn = random_distr(l)
+        l[rrsn],l[-1] = l[-1],l[rrsn]
+        yield l.pop()[0]
+
+for i in generate_random(l):
+    print i
+
